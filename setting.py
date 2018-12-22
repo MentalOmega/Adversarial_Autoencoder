39cd0325bdb3aef3243f50b5c9f777c673c5cc13
centos105 <centos@105.com>
2018-12-22 16:21:56 +0800

    对抗自编码和DCGAN

--- setting.py
+++ setting.py
@@ -1,9 +1,9 @@
 z_dim = 2
-batch_size = 64
+batch_size = 128
 image_w = 28
 image_h = 28
 image_d = 1
 input_dim = 784
-n_epochs = 100
-learning_rate = 0.001
+n_epochs = 200
+learning_rate = 0.002
 beta1 = 0.9
